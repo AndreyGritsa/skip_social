d2adf6229406abc43f5085380e811b40630222c0
Andrey Gritsaenko <gricaenko.95a@gmail.com>
2024-11-18 09:45:11 +0100

    clean friends feature from fakeData, update tests

--- frontend/src/features/friends/friendsSlice.spec.ts
+++ frontend/src/features/friends/friendsSlice.spec.ts
@@ -1,13 +1,28 @@
-import friendsReducer, { FriendsState } from "./friendsSlice";
-import { fakeFriendsData } from "./friendsSlice";
+import friendsReducer, {
+  FriendsState,
+  setFriends,
+  Friend,
+} from "./friendsSlice";
 
 describe("friends reducer", () => {
   const initialState: FriendsState = {
-    friends: fakeFriendsData,
+    friends: [],
   };
   it("should return the initial state", () => {
     expect(friendsReducer(undefined, { type: "unknown" })).toEqual(
       initialState
+    );
+  });
+  it("should handle setFriends", () => {
+    const newFriends: Friend[] = [
+      { id: "1", name: "Alice", status: "online" },
+      { id: "2", name: "Bob", status: "away" },
+    ];
+    const expectedState: FriendsState = {
+      friends: newFriends,
+    };
+    expect(friendsReducer(initialState, setFriends(newFriends))).toEqual(
+      expectedState
     );
   });
 });
--- frontend/src/features/friends/friendsSlice.ts
+++ frontend/src/features/friends/friendsSlice.ts
@@ -1,12 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "../user/userSlice";
 
-export const fakeFriendsData = [
-  { id: "1", name: "Alice", status: "online" },
-  { id: "2", name: "Bob", status: "away" },
-  { id: "500", name: "NotExistentChannel", status: "online" },
-];
-
 export interface Friend extends User {
   status: string;
 }
